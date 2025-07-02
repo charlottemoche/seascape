1b0a84d7db25a44feeee2faf555e733f57633e14
Charlotte Moche <charlottebmoche@gmail.com>
2025-07-02 14:39:06 -0400

    tip

--- components/TipCard.tsx
+++ components/TipCard.tsx
@@ -3,6 +3,12 @@ import { View, Text, Button } from '@/components/Themed';
 import { useTipPurchase } from '@/hooks/useTipPurchase';
 import { StyleSheet } from 'react-native';
 import { Loader } from '@/components/Loader';
+import { Image } from 'react-native';
+import rainbowFish from '@/assets/images/rainbow-fish.png';
+import coloredFish from '@/assets/images/colored-fish.png';
+
+const COLORED_FISH = coloredFish;
+const RAINBOW_FISH = rainbowFish;
 
 export default function TipCard() {
   const { loading, processing, error, buyTip, price } = useTipPurchase();
@@ -14,6 +20,13 @@ export default function TipCard() {
       <Text style={styles.text}>
         This app was made by one person, slowly and with love. No subscriptions, just a tiny project from me to you. Tips help me keep it alive!
       </Text>
+      <Text style={styles.text}>
+        Your gift will unlock two new fish colors.
+      </Text>
+      <View style={styles.imageContainer}>
+        <Image source={COLORED_FISH} style={styles.image} />
+        <Image source={RAINBOW_FISH} style={styles.image} />
+      </View>
 
       <Button
         title={`Buy me a coffee (${price})`}
@@ -44,5 +57,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     lineHeight: 20,
     marginBottom: 12,
+  },
+  imageContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    gap: 12,
+    backgroundColor: 'transparent',
+  },
+  image: {
+    width: 40,
+    height: 40,
+    marginBottom: 6,
   },
 });
